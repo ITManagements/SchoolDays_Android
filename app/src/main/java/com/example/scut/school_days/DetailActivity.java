3af317686bc5f385f05e5c3a320155a51e4f85bc
zyssky <2462084760@qq.com>
2016-11-01 23:20:57 +0800

    日程的增删改查，登录界面，部分设定界面

--- app/src/main/java/com/example/scut/school_days/DetailActivity.java
+++ app/src/main/java/com/example/scut/school_days/DetailActivity.java
@@ -1,55 +1,95 @@
 package com.example.scut.school_days;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Date;
 
 public class DetailActivity extends AppCompatActivity {
 
     public final String TAG = DetailActivity.class.getSimpleName();
-
-    private Button submit_but;
-
     private Intent intent;
 
-    private EditText detail_title;
+    private EditText activity_content_edittext;
+    private Button pick_time_btn;
 
-    private EditText detail_content;
+    final String activity_name = "activity_name";
+    final String activity_start_time = "strat_time";
+    final String activity_start_time_hour = "hour";
+    final String activity_start_time_minute = "minute";
+    final String position_str = "pos";
+
+//    private String time;
+    private int pick_hour;
+    private int pick_minute;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        context=this;
 
-        submit_but = (Button) findViewById(R.id.submit_but);
+        pick_time_btn = (Button) findViewById(R.id.pick_time);
+        activity_content_edittext = (EditText) findViewById(R.id.activity_content);
 
-        detail_title = (EditText) findViewById(R.id.detail_title);
-
-        detail_content = (EditText) findViewById(R.id.detail_content);
+        pick_time_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        pick_hour = hourOfDay;
+                        pick_minute = minute;
+                        pick_time_btn.setText(Format.formatRemindTitle(hourOfDay,minute));
+                    }
+                }, new Date().getHours(),new Date().getMinutes(),true).show();
+            }
+        });
 
         intent = this.getIntent();
         Bundle bundle = intent.getExtras();
 
+        if(bundle!=null) {
+            pick_hour = bundle.getInt(activity_start_time_hour);
+            pick_minute = bundle.getInt(activity_start_time_minute);
+            activity_content_edittext.setText(bundle.getString(activity_name));
+            pick_time_btn.setText(Format.formatRemindTitle(pick_hour,pick_minute));
 
-        detail_title.setText(bundle.getString("title"));
-        detail_content.setText(bundle.getString("content"));
+        }
 
-        submit_but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: "+detail_title.getText());
-                intent.putExtra("title",detail_title.getText().toString());
-                intent.putExtra("content",detail_content.getText().toString());
-                setResult(RESULT_OK,intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.confirm:
+                intent.putExtra(activity_start_time_hour,pick_hour);
+                intent.putExtra(activity_start_time_minute,pick_minute);
+                intent.putExtra(activity_name, activity_content_edittext.getText().toString());
+                setResult(RESULT_OK, intent);
                 finish();
-            }
-        });
-
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/scut/school_days/MainActivity.java
+++ app/src/main/java/com/example/scut/school_days/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.scut.school_days;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.AdapterView;
@@ -23,6 +30,7 @@ import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,13 +46,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ListView listView;
 
-    private Button button;
+    private Button add_button;
 
     private SimpleAdapter adapter;
 
     private List<Map<String,Object>> listitem;
 
     private Button today;
+    private int choose_item_position;
+
+    final String activity_name = "activity_name";
+    final String activity_start_time = "strat_time";
+    final String activity_start_time_hour = "hour";
+    final String activity_start_time_minute = "minute";
+    final String position_str = "pos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,65 +72,122 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         calendarView.setSelectedDate(CalendarDay.today());
         calendarView.setOnMonthChangedListener(this);
         calendarView.setDynamicHeightEnabled(true);
-        calendarView.setTileHeightDp(32);
-        calendarView.setTileWidthDp(50);
-        getSupportActionBar().setTitle(FORMATTER.format(CalendarDay.today().getDate()));
+        calendarView.setDateTextAppearance(android.R.style.TextAppearance_Material_Small);
+        calendarView.setTileHeightDp(35);
+        calendarView.setTileWidthDp(53);
+        getSupportActionBar().setTitle(Format.formatDateTitle(CalendarDay.today()));
 
         listitem=new ArrayList<Map<String, Object>>();
-
         adapter = new SimpleAdapter(this,listitem,R.layout.list_item,
-                new String[]{"title","content"},new int[]{R.id.title,R.id.content});
-
+                new String[]{activity_name,activity_start_time},new int[]{R.id.acitvity_name,R.id.activity_start_time});
         listView = (ListView) findViewById(R.id.listview);
-
         listView.setAdapter(adapter);
 
+        add_button = (Button) findViewById(R.id.add_but);
+        add_button.setOnClickListener(this);
+        today=(Button) findViewById(R.id.today);
+        today.setOnClickListener(this);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                choose_item_position = position;
+                deleteItem();
+                return true;
+            }
+        });
+
+
+        // intent's bundle include position activity_name start_time
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Object item = adapter.getItem(position);
-                TextView title = (TextView) view.findViewById(R.id.title);
-                TextView content = (TextView) view.findViewById(R.id.content);
+                Map<String,Object> map = listitem.get(position);
+
+//                TextView name= (TextView) view.findViewById(R.id.acitvity_name);
+//                TextView time = (TextView) view.findViewById(R.id.activity_start_time);
                 Intent intent = new Intent(getApplicationContext(),DetailActivity.class);
-                intent.putExtra("title",title.getText());
-                intent.putExtra("content",content.getText());
-                intent.putExtra("position",position);
+                intent.putExtra(activity_name, (String) map.get(activity_name) );
+                intent.putExtra(activity_start_time_hour, (Integer) map.get(activity_start_time_hour));
+                intent.putExtra(activity_start_time_minute,(Integer) map.get(activity_start_time_minute));
+                intent.putExtra(position_str,position);
                 startActivityForResult(intent,0);
             }
         });
 
-        button = (Button) findViewById(R.id.add_but);
-
-        button.setOnClickListener(this);
-
-        today=(Button) findViewById(R.id.today);
-
-        today.setOnClickListener(this);
-
     }
 
-//    private List<Map<String,Object>> getData(CalendarDay day){
-//        List<Map<String,Object>> data = new ArrayList<Map<String, Object>>();
-//
-//        Map<String,Object> map = new HashMap<String, Object>();
-//        map.put("title","big title");
-//        map.put("content","small content");
-//        data.add(map);
-//
-//        return  data;
-//    }
 
-    private Map<String,Object> generate_item(String title,String content){
+
+    void deleteItem(){
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle(R.string.longclick_ask_title).setMessage(
+                R.string.longclick_ask_message).setPositiveButton(R.string.longclick_ask_positive_btn,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listitem.remove(choose_item_position);
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton(R.string.longclick_ask_negative_btn,null).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sync:
+                break;
+            case R.id.restore:
+                break;
+            case R.id.arrange:
+                break;
+            case R.id.jump:
+                DatePickerDialog datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        calendarView.setCurrentDate(CalendarDay.from(year,monthOfYear,dayOfMonth));
+                        calendarView.setSelectedDate(CalendarDay.from(year,monthOfYear,dayOfMonth));
+                    }
+                },CalendarDay.today().getYear(),CalendarDay.today().getMonth(),CalendarDay.today().getDay());
+
+                datePicker.show();
+                break;
+            case R.id.settings:
+                startActivity(new Intent(getApplicationContext(),ScheduleActivity.class));
+                break;
+            case R.id.log:
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private Map<String,Object> generate_item(String title,int hour,int minute){
         Map<String,Object> map = new HashMap<String, Object>();
-        map.put("title",title);
-        map.put("content",content);
+        map.put(activity_name,title);
+        map.put(activity_start_time_hour,hour);
+        map.put(activity_start_time_minute,minute);
+        map.put(activity_start_time,Format.formatRemindTitle(hour,minute));
         return map;
     }
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @Nullable CalendarDay date, boolean selected) {
-        System.out.println("selected day");
+        loadSelectedDaySchedule(date);
+    }
+
+    void loadSelectedDaySchedule(CalendarDay selectday){
+        listitem.clear();
+        adapter.notifyDataSetChanged();
     }
 
 
@@ -123,12 +195,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add_but:
-                CalendarDay day = calendarView.getSelectedDate();
+                startActivityForResult(new Intent(getApplicationContext(),DetailActivity.class),1);
+//                CalendarDay day = calendarView.getSelectedDate();
 
-                Map<String, Object> map = generate_item(day.toString(), "content");
-                listitem.add(map);
-                adapter.notifyDataSetChanged();
-                Log.d(LOG_TAG,"add_but clicked");
+//                Map<String, Object> map = generate_item(day.toString(), "content");
+//                listitem.add(map);
+//                adapter.notifyDataSetChanged();
+//                Log.d(LOG_TAG,"add_but clicked");
                 break;
             case R.id.today:
                 CalendarDay currentmonth = CalendarDay.today();
@@ -140,23 +213,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        switch (resultCode){
-            case RESULT_OK:
-                Bundle bundle = data.getExtras();
-                setItem(bundle.getInt("position"),bundle.getString("title"),bundle.getString("content"));
+        if(resultCode==RESULT_OK){
+            Bundle bundle = data.getExtras();
+            switch (requestCode){
+                case 0:
+                    setItem(bundle.getInt(position_str),bundle.getString(activity_name),bundle.getInt(activity_start_time_hour),bundle.getInt(activity_start_time_minute));
+                    break;
+                case 1:
+                    setItem(-1,bundle.getString(activity_name), bundle.getInt(activity_start_time_hour),bundle.getInt(activity_start_time_minute));
+                    break;
+            }
         }
+
     }
 
-    private void setItem(int position,String title,String content){
-        Map<String, Object> map = generate_item(title, content);
-        listitem.set(position,map);
+    private void setItem(int position,String title,int hour,int minute){
+        Map<String, Object> map = generate_item(title, hour,minute);
+        if(position==-1)
+            listitem.add(map);
+        else
+            listitem.set(position,map);
         adapter.notifyDataSetChanged();
     }
 
 
     @Override
     public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-        getSupportActionBar().setTitle(FORMATTER.format(date.getDate()));
+        String s = Format.formatDateTitle(date.getYear(),date.getMonth());
+//        calendarView.setCurrentDate(CalendarDay.from(date.getYear(),date.getMonth(),CalendarDay.today().getDay()));
+//        calendarView.setSelectedDate(CalendarDay.from(date.getYear(),date.getMonth(),CalendarDay.today().getDay()));
+        getSupportActionBar().setTitle(s);
     }
 }
